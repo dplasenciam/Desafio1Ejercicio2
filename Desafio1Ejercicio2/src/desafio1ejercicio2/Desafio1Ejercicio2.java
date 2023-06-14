e6a444e364b76749db94523a89e156a12bc4e398
dplasenciam <daniel.plasencia@pucp.pe>
2023-06-14 18:27:32 -0500

    java

---
+++ Desafio1Ejercicio2/src/desafio1ejercicio2/Desafio1Ejercicio2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desafio1ejercicio2;
+
+/**
+ *
+ * @author Daniel Plasencia
+ */
+public class Desafio1Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
